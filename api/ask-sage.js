29ba14b39b709ccd0ae4c650018c63e3e22eb6db
Integrity-Exists.github.io <svc.with.integrity@gmail.com>
2025-07-31 10:46:46 -0400

    Update ask-sage.js

--- api/ask-sage.js
+++ api/ask-sage.js
@@ -1,4 +1,6 @@
 // File: api/ask-sage.js 
+import fetch from node-fetch;
+
 export default async function handler(req, res) { 
 if (req.method !== 'POST') { 
 return res.status(405).json({ error: 'Method Not Allowed' }); 
